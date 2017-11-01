d9931e77a61469d88afb3259a8fc9ddbe27ea4e0
Daniel Serkowski <zarbonix@gmail.com>
2017-11-01 19:39:47 +0100

    Initial commit

---
+++ docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#mkdir ~/tmpInstalation
+#cd ~/tmpInstalation
+
+# docker
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo systemctl enable docker
+
+#rm -r ~/tmpInstalation
+

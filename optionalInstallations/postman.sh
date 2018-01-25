7654a99c449804c5b5a9f3bb15c8908cf49ca3f2
Daniel Serkowski <zarbonix@gmail.com>
2018-01-25 14:19:59 +0100

    Added postman

---
+++ optionalInstallations/postman.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo tar -xzf postman.tar.gz -C /opt
+rm postman.tar.gz
+sudo ln -s /opt/Postman/Postman /usr/bin/postman

3c613527c89995302f47c7b146da7e4fee118317
Daniel Serkowski <serkowskidaniel@gmail.com>
2018-08-04 02:53:03 +0200

    Fingerprint: disabled unwanted timeout message

--- thinkpad/fingerprint.sh
+++ thinkpad/fingerprint.sh
@@ -3,3 +3,5 @@ sudo apt update
 sudo apt install -y libpam-fprintd
 fprintd-enroll
 
+# disable timeout message on the login screen
+sudo sed -i -e 's/timeout=10/timeout=-1/g' /etc/pam.d/common-auth

1ee2c72b7e65167503319744ce005082b25319f0
Daniel Serkowski <dserkowski@soldevelo.com>
2019-03-19 17:49:34 +0100

    Added gradle and Java 8 Oracle

--- all.sh
+++ all.sh
@@ -7,18 +7,21 @@ sudo apt-get update
 . redshift.sh
 . psensor.sh
 . wifiIssues.sh
+. installJava8Oracle.sh
 
 sudo apt -y install chromium-browser
 sudo apt -y install maven
 sudo apt -y install vim
-sudo apt -y install default-jre
-sudo apt -y install default-jdk
+#sudo apt -y install default-jre
+#sudo apt -y install default-jdk
+
 sudo apt -y install openjfx # JavaFX
 sudo apt -y install mysql-workbench # TODO: to replace
 sudo apt -y install mc
 sudo apt -y install terminator
 gsettings set org.gnome.desktop.default-applications.terminal exec terminator # terminator as default
 sudo apt -y install npm
+sudo apt -y install gradle
 
 sudo snap install slack --classic
 sudo snap install intellij-idea-community --classic --edge

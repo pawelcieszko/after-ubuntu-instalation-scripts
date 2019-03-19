6e624bbae2a2acb0b031d82d49341279ba10a7ae
Daniel Serkowski <dserkowski@soldevelo.com>
2019-03-19 14:57:40 +0100

    Terminator rightclick fix & wired network as default fix

--- all.sh
+++ all.sh
@@ -17,7 +17,14 @@ sudo apt -y install openjfx # JavaFX
 sudo apt -y install mysql-workbench # TODO: to replace
 sudo apt -y install mc
 sudo apt -y install terminator
+gsettings set org.gnome.desktop.default-applications.terminal exec terminator # terminator as default
+sudo apt -y install npm
+
 sudo snap install slack --classic
 sudo snap install intellij-idea-community --classic --edge
 sudo snap install skype --classic
 sudo snap install --classic vscode
+
+# wired network as default (wireless is disabled when plugged)
+sudo cp data/99-disable-wireless-when-wired.sh /etc/NetworkManager/dispatcher.d/99-disable-wireless-when-wired.sh
+sudo chmod +x /etc/NetworkManager/dispatcher.d/99-disable-wireless-when-wired.sh

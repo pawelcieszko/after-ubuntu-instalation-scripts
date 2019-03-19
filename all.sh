2c1225c71cef9284380e0845526d884d1d7a743f
Daniel Serkowski <dserkowski@soldevelo.com>
2019-03-19 13:27:55 +0100

    Update

--- all.sh
+++ all.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 . git.sh
 . redshift.sh
 . psensor.sh
-. resumeWifiIssueFix.sh
+. wifiIssues.sh
 
 sudo apt -y install chromium-browser
 sudo apt -y install maven
@@ -14,6 +14,9 @@ sudo apt -y install vim
 sudo apt -y install default-jre
 sudo apt -y install default-jdk
 sudo apt -y install openjfx # JavaFX
-sudo apt -y install mysql-workbench
+sudo apt -y install mysql-workbench # TODO: to replace
 sudo apt -y install mc
 sudo apt -y install terminator
+sudo snap install slack --classic
+sudo snap install intellij-idea-community --classic --edge
+sudo snap install skype --classic
---
+++ optionalInstallations/chrome.sh
@@ -0,0 +1,5 @@
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+rm google-chrome-stable_current_amd64.deb
